3f835615beff7bfdc0b4e450dea50f971449a98f
Bpachecoalfaro <bpachecoalfaro@gmail.com>
2024-02-23 15:07:45 -0300

    Mongodb implementado

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { Server } from "./presentation/server";
 import { envs } from './config/plugins/envs.plugin';
+import { LogModel, MongoDatabase } from './data/mongo';
 
 
 (async() => {
@@ -8,7 +9,24 @@ import { envs } from './config/plugins/envs.plugin';
 })();
 
 
-function main(){
+async function main(){
+    
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME,
+    });
+
+    // // Crear un coleccion = tables, | documento = registro
+    // const newLog = await LogModel.create({
+    //     message: 'Test message desde Mongo',
+    //     orign: 'App.ts',
+    //     level: 'low',
+    // });
+
+    // await newLog.save();
+    // console.log( newLog );
+    // const logs = await LogModel.find();
+    // console.log( logs[1]);
 
     Server.start()
     // console.log( envs );
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -5,44 +5,47 @@ import { envs } from '../config/plugins/envs.plugin';
 import { CronService } from './cron/cron-service';
 import { EmailService } from './email/email.service';
 import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
+import { MongoLogDataSource } from '../infrastructure/datasources/mongo-log.datasource';
+import { LogSeverityLevel } from '../domain/entities/log.entity';
 
-const fileSystemLogRepository = new LogRepositoryImpl(
-    new FileSystemDatasource()
+const logRepository = new LogRepositoryImpl(
+    new FileSystemDatasource(),
+    // new MongoLogDataSource(),
 );
 const emailService = new EmailService();
 
 
 export class Server {
 
-    public static start() {
+    public static async start() {
 
         console.log('Server started...');
 
-        // TODO: Mandar email
-        new SendEmailLogs(
-            emailService,
-            fileSystemLogRepository,
-        ).execute(
-            ['xxxxxxxxx@gmail.com','xxxxxxxx@gmail.com']
-            )
+        // // TODO: Mandar email
+        // new SendEmailLogs(
+        //     emailService,
+        //     fileSystemLogRepository,
+        // ).execute(
+        //     ['xxxxxxxxx@gmail.com','xxxxxxxx@gmail.com']
+        //     )
         
         // emailService.sendEmailWithFileSystemLogs(
         //     ['bpachecoalfaro@gmail.com','ambar.aguilar.c@gmail.com']
         // );
 
+        const logs = await logRepository.getLogs(LogSeverityLevel.low);
+        console.log( logs );
+
         // CronService.createJob(
         //     '*/5 * * * * *',
         //     () => {
-        //         const url = 'http://localhost:3000/';
+        //         const url = 'http://lasdasdqwe.com/';
         //         new CheckService(
-        //             fileSystemLogRepository,
+        //             logRepository,
         //             () => console.log( `${ url } is ok` ),
         //             ( error )  => console.log( error ),
         //         ).execute( url );
         //         // new CheckService().execute( 'http://localhost:3000/');
-
-
-                
 
         //     }
         // );
